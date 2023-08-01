92d3d837adccddf0853cfa673b71e53455746346
JaiderValencia <janvame05@gmail.com>
2023-07-31 23:01:22 -0500

    agrego paginacion

--- src/controllers/invitationController.ts
+++ src/controllers/invitationController.ts
@@ -5,6 +5,12 @@ import path from 'path'
 import { SupplierInvitation } from "../database/models/SupplierInvitation"
 import { Supplier } from "../database/models/Supplier"
 
+interface RequestQuery extends Request {
+    query: {
+        page: string,
+    }
+}
+
 const controller = {
     postInvitation: (req: Request, res: Response) => {
         if (!req.file) {
@@ -26,7 +32,7 @@ const controller = {
             supplier_id: number,
             commerce_cell_phone: string
         }
-        
+
         fileParsed.forEach(async (invitation: Invitation) => {
             const supplier = await Supplier.findByPk(invitation.supplier_id)
 
@@ -36,15 +42,25 @@ const controller = {
                     commerce_cell_phone: invitation.commerce_cell_phone
                 })
             }
-        });        
+        });
 
         res.status(200).json({
             code: 200,
             message: 'invitation created'
         })
     },
-    getInvitations: async (req: Request, res: Response) => {
-        const invitations: SupplierInvitation[] = await SupplierInvitation.findAll({ include: { all: true } })
+    getInvitations: async (req: RequestQuery, res: Response) => {
+        const totalInvitationsInDB: number = await SupplierInvitation.count()
+
+        const limit = 10;
+
+        const page = parseInt(req.query.page) || 1
+
+        const offset = limit * (page - 1);
+
+        const limitPag = Math.ceil(totalInvitationsInDB / limit);
+
+        const invitations: SupplierInvitation[] = await SupplierInvitation.findAll({ limit, offset, include: { all: true } })
 
         interface InvitationsParsedArray {
             supplierId: number,
@@ -62,13 +78,41 @@ const controller = {
             })
         })
 
-        res.status(200).json({
+        interface JsonRes {
+            apiResponse: {
+                code: number,
+                totalInvitationsInDB: number
+                count: number
+            },
+            actions: {
+                limit: number,
+                next?: string,
+                previous?: string
+            },
+            list: object[]
+        }
+
+        const jsonRes: JsonRes = {
             apiResponse: {
                 code: 200,
-                total: invitations.length
+                totalInvitationsInDB,
+                count: invitationsParsed.length
+            },
+            actions: {
+                limit,
             },
             list: [...invitationsParsed]
-        })
+        }
+
+        if (page >= 1 && invitationsParsed.length == limit && page < limitPag) {
+            jsonRes.actions.next = `${req.protocol}://${req.get('host')}/invitation/?page=${(page + 1)}`;
+        }
+
+        if (page > 1) {
+            jsonRes.actions.previous = `${req.protocol}://${req.get('host')}/invitation/?page=${(page - 1)}`;
+        }
+
+        res.status(200).json(jsonRes)
     }
 }
 

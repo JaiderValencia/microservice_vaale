eb6cf116cbac253311858303aef18369c4d3869d
JaiderValencia <janvame05@gmail.com>
2023-07-31 19:48:11 -0500

    agrego el posteo de datos con el CSV y el list

--- src/controllers/invitationController.ts
+++ src/controllers/invitationController.ts
@@ -14,17 +14,48 @@ const controller = {
             columns: true
         })
 
-        res.send(fileParsed)
+        interface Invitation {
+            supplier_id: number,
+            commerce_cell_phone: string
+        }
+
+        fileParsed.forEach(async (invitation: Invitation) => {
+            await SupplierInvitation.create({
+                supplier_id: invitation.supplier_id,
+                commerce_cell_phone: invitation.commerce_cell_phone
+            })
+        });
+
+        res.status(200).json({
+            code: 200,
+            message: 'invitation created'
+        })
     },
     getInvitations: async (req: Request, res: Response) => {
-        const invitations: SupplierInvitation[] = await SupplierInvitation.findAll()
+        const invitations: SupplierInvitation[] = await SupplierInvitation.findAll({ include: { all: true } })
+
+        interface InvitationsParsedArray {
+            supplierId: number,
+            supplierName: string,
+            commerce_cell_phone: string
+        }
+
+        const invitationsParsed: Array<InvitationsParsedArray> = []
+
+        invitations.forEach(invitation => {
+            invitationsParsed.push({
+                supplierId: invitation.supplier.id,
+                supplierName: invitation.supplier.name,
+                commerce_cell_phone: invitation.commerce_cell_phone
+            })
+        })
 
         res.status(200).json({
             apiResponse: {
                 code: 200,
                 total: invitations.length
             },
-            list: [...invitations]
+            list: [...invitationsParsed]
         })
     }
 }
